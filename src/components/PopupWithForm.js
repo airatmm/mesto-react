670d50f61d03c3281bb82afa531742ea5e2a2d2f
Ayrat Mukhametshin <airatmm@gmail.com>
2022-03-23 10:47:10 +0300

    10 sprint after review

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -2,15 +2,11 @@ function PopupWithForm({name, title, children, buttonText, isOpen, onClose}) {
 return(
     <div className={`popup popup_type_${name} ${isOpen && 'popup_opened'}`}>
         <div className="popup__content">
-            <button className={`popup__close popup__close_${name}`} type="button"  onClick={onClose}>
-
-            </button>
+            <button className={`popup__close popup__close_${name}`} type="button"  onClick={onClose} />
             <h2 className="popup__title">{title}</h2>
             <form className={`popup__form popup__form__${name}`} name="form_profile" noValidate>
                 {children}
-                <button type="submit" className="popup__button popup__button_edit_save" name="save">
-                    {buttonText}
-                </button>
+                <button type="submit" className="popup__button popup__button_edit_save" name="save">{buttonText}</button>
             </form>
         </div>
     </div>
